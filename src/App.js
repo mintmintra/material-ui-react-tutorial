1719475f44580958d5299cc654b127b74844ec93
mintmintra <mintracss019@gmail.com>
2022-05-23 22:54:15 +0700

    checkbox

--- src/App.js
+++ src/App.js
@@ -5,11 +5,32 @@ import './App.css';
 import logo from './logo.svg';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel 
+      control={<Checkbox
+        checked={checked}
+        icon={<SaveIcon />}
+        checkedIcon={<SaveIcon />}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label' : 'error checkbox'
+        }}
+      />}
+      label="Testing Checkbox"
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" color="primary">
           <Button
           startIcon={<SaveIcon />}
